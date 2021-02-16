048c329becd459708cdbfe3e6102ca5b607ecaed
emericen <liang.741@buckeyemail.osu.edu>
2021-02-16 16:23:08 +0800

    restructured templates

--- flashcards/models.py
+++ flashcards/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.utils import timezone
+# from django.urls import reverse
+
+
 
 # Create your models here.
 class Flashcard(models.Model):
@@ -9,3 +13,5 @@ class Flashcard(models.Model):
 	def __str__(self):
 		pass
 
+	# def get_absolute_url(self):
+	# 	return reverse('post-detail', kwargs={'pk': self.pk})
--- users/models.py
+++ users/models.py
@@ -1,23 +1,33 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 from flashcards.models import Flashcard
+
+
+
+
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	last_name = models.CharField(max_length=100)
 	first_name = models.CharField(max_length=100)
+	owned_flashcards = models.ManyToManyField(Flashcard, through='Ownership')
 	date_joined = models.DateTimeField(default=timezone.now)
 
 	def __str__(self):
-		pass
+		return f'{self.last_name + self.first_name} Profile'
 
 
-class Owns(models.Model):
+
+
+class Ownership(models.Model):
 	Owner = models.ForeignKey(User, on_delete=models.CASCADE)
 	Flashcard = models.ForeignKey(Flashcard, on_delete=models.CASCADE)
+	date_joined = models.DateTimeField(default=timezone.now)
 
 	def __str__(self):
-		pass
+		return '(' + Owner + ',' + Flashcard + ')'
+
 
 
 
