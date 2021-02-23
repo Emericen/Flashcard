967766c06bda9d913960ab3853f137b8656519d9
emericen <liang.741@buckeyemail.osu.edu>
2021-02-23 23:20:32 +0800

    Authentication complete. Only small UI problems(such as validation error text) left to be fixed

--- flashcard_project/urls.py
+++ flashcard_project/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('home/', user_views.home, name='home'),
     path('register/', user_views.register, name='register'),
     path('login/', user_views.Login.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    # path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('logout/', user_views.logout, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate
+from django.contrib.auth import logout as django_logout
 from .forms import UserCreationForm, UserLoginForm
 import sys
 
 
 def home(request):
 	if not request.user.is_authenticated:
-		return redirect('register')
+		return redirect('login')
 	else:
 		return render(request, 'users/home.html')
 
@@ -33,5 +33,9 @@ class Login(LoginView):
 	authentication_form = UserLoginForm
 
 
+def logout(request):
+	django_logout(request)
+	messages.info(request, f'您已退出登录 要想再用就他妈先登录')
+	return redirect('login')
 
 
